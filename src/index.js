d66fc07c0df886b4d11c6265567705cdb10af4dc
Ramok <steamxaiden@gmail.com>
2021-09-16 00:14:10 +0200

    Premier et normalement dernier commit sah

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args))
+const {WebSocket} = require("ws");
+const fs = require("fs");
+const dotenv = require("./lib/dotenv");
+
+var env = dotenv(fs.readFileSync("./.env", {"encoding": "utf-8"}));
+  (async() => {
+
+    Object.keys(env).forEach(function (key) {
+        if (!Object.prototype.hasOwnProperty.call(process.env, key)) {
+          process.env[key] = env[key]
+        }
+      })
+    function initSltcv() {
+        var socket2 = new WebSocket("ws://localhost:3000");
+
+        socket2.on("close", () => {
+            setTimeout(initSltcv, 5000);
+        });
+        socket2.on("open", () => {
+            console.log("Websocket started")
+        });
+
+        socket2.onmessage = function(msg) {
+            var blinfo = JSON.parse(msg.data);
+            switch(blinfo.message) {
+                case "removed blacklist":
+                    fetch("https://discord.com/api/v9/channels/887810878928478239/messages",{headers:{Authorization:"Bot "+process.env.TOKEN,"Content-Type":"application/json"},method:"POST",body:JSON.stringify({content:"Quelqu'un a été unblacklist !",embeds:[{color:5420936,fields:[{name:"Membre",value:`${null==blinfo.blacklisteduser.username?blinfo._id:`\`${blinfo.blacklisteduser.username}#${blinfo.blacklisteduser.discriminator}\` \n (**${blinfo._id}**)`}`},{name:"Reason",value:blinfo.reason}],footer:{text:`Unblacklisted by ${blinfo.blacklistedby}`},author:{name:"NLAR | BLACKLIST MANAGER"},thumbnail:{url:`${null==blinfo.blacklisteduser.avatar?"":`https://cdn.discordapp.com/avatars/${blinfo._id}/${blinfo.blacklisteduser.avatar}?size=128`}`}}]})});
+                    break;
+                case "new blacklist":
+                    fetch("https://discord.com/api/v9/channels/887810878928478239/messages",{headers:{Authorization:"Bot "+process.env.TOKEN,"Content-Type":"application/json"},method:"POST",body:JSON.stringify({content:"Quelqu'un a été blacklist !",embeds:[{color:13565962,fields:[{name:"Membre",value:`${null==blinfo.blacklisteduser.username?blinfo._id:`\`${blinfo.blacklisteduser.username}#${blinfo.blacklisteduser.discriminator}\` \n (**${blinfo._id}**)`}`},{name:"Reason",value:blinfo.reason}],footer:{text:`Blacklisted by ${blinfo.blacklistedby}`},author:{name:"NLAR | BLACKLIST MANAGER"},thumbnail:{url:`${null==blinfo.blacklisteduser.avatar?"":`https://cdn.discordapp.com/avatars/${blinfo._id}/${blinfo.blacklisteduser.avatar}?size=128`}`}}]})});
+                    break;
+            }
+        }
+      
+    }
+    initSltcv();
+})();
